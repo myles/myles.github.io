2f1da9aee312f17b3e92084bedbd61ec856e872d
Myles Braithwaite <me@mylesbraithwaite.com>
2015-10-13 13:59:23 -0400

    Updates

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,7 +1,7 @@
 (function() {
   $(function() {
     var api_url, articleRepo, source, template;
-    source = '<ul class="bullets">' + '{{#each data}}' + '{{#unless fork}}' + '<li class="bullet">' + '<div class="bullet-content">' + '<a href="{{html_url}}">' + '<h2>{{full_name}}</h2>' + '</a>' + '<p>{{description}}</p>' + '</div>' + '</li>' + '{{/unless}}' + '{{/each}}' + '</ul>';
+    source = '<ul class="bullets">' + '{{#each data}}' + '{{#unless fork}}' + '<li class="bullet">' + '<div class="bullet-content">' + '<a href="{{html_url}}">' + '<h2>' + '{{full_name}} ' + '{{#if language}}<em>({{language}})</em>{{/if}}' + '</h2>' + '</a>' + '<ul class="bullet-links">' + '{{#if homepage}}' + '<li>' + '<a href="{{homepage}}">' + '<img src="/assets/images/link-external.svg" title="Homepage">' + '</a>' + '</li>' + '{{/if}}' + '{{#if has_wiki}}' + '<li>' + '<a href="{{html_url}}/wiki">' + '<img src="/assets/images/book.svg" title="Wiki">' + '</a>' + '</li>' + '{{/if}}' + '{{#if has_issues}}' + '<li>' + '<a href="{{html_url}}/issues">' + '<img src="/assets/images/issue-opened.svg" title="Wiki">' + '</a>' + '</li>' + '{{/if}}' + '</ul>' + '<p>{{description}}</p>' + '</div>' + '</li>' + '{{/unless}}' + '{{/each}}' + '</ul>';
     api_url = 'https://api.github.com/users/myles/repos?sort=updated&type=all&callback=?';
     template = Handlebars.compile(source);
     articleRepo = $('#js-github-projects');
