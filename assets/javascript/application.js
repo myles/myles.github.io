8b7e4c82c460a6e05f42e0011166bb3e66b53ee3
Myles Braithwaite <me@mylesbraithwaite.com>
2015-10-18 15:49:17 -0400

    Updates

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,12 +1,15 @@
 (function() {
   $(function() {
-    var api_url, articleRepo, source, template;
+    var articleArchiveRepo, articleRepo, source, template;
     source = '<ul class="bullets">' + '{{#each data}}' + '{{#unless fork}}' + '<li class="bullet">' + '<div class="bullet-content">' + '<a href="{{html_url}}">' + '<h2>' + '{{full_name}} ' + '{{#if language}}<em>({{language}})</em>{{/if}}' + '</h2>' + '</a>' + '<ul class="bullet-links">' + '<li>' + '<a href="{{html_url}}">' + '<img src="/assets/images/octoface.svg" title="GitHub">' + '</a>' + '</li>' + '{{#if homepage}}' + '<li>' + '<a href="{{homepage}}">' + '<img src="/assets/images/link-external.svg" title="Homepage">' + '</a>' + '</li>' + '{{/if}}' + '{{#if has_wiki}}' + '<li>' + '<a href="{{html_url}}/wiki">' + '<img src="/assets/images/book.svg" title="Wiki">' + '</a>' + '</li>' + '{{/if}}' + '{{#if has_issues}}' + '<li>' + '<a href="{{html_url}}/issues">' + '<img src="/assets/images/issue-opened.svg" title="Wiki">' + '</a>' + '</li>' + '{{/if}}' + '{{#if has_downloads}}' + '<li>' + '<a href="{{html_url}}/releases">' + '<img src="/assets/images/desktop-download.svg" title="Downloads">' + '</a>' + '</li>' + '{{/if}}' + '</ul>' + '<p>{{description}}</p>' + '</div>' + '</li>' + '{{/unless}}' + '{{/each}}' + '</ul>';
-    api_url = 'https://api.github.com/users/myles/repos?sort=updated&type=all&callback=?';
     template = Handlebars.compile(source);
     articleRepo = $('#js-github-projects');
-    jQuery.getJSON(api_url, function(data) {
+    articleArchiveRepo = $('#js-github-archive-projects');
+    jQuery.getJSON('https://api.github.com/users/myles/repos?sort=updated&type=all&callback=?', function(data) {
       articleRepo.html(template(data));
+    });
+    jQuery.getJSON('https://api.github.com/orgs/myles-archive/repos?sort=updated&type=all&callback=?', function(data) {
+      articleArchiveRepo.html(template(data));
     });
   });
 
