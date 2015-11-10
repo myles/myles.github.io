d4ffdafe5c7e28d0e5fb0cfde8742bf6bc3e3544
Myles Braithwaite <me@mylesbraithwaite.com>
2015-11-10 12:02:29 -0500

    Adding a user and email thing.

--- tasks/gh-pages.js
+++ tasks/gh-pages.js
@@ -1,6 +1,10 @@
 module.exports = function (grunt) {
     var config = {
         options: {
+            user: {
+              name: 'Deploy',
+              email: 'me+deploy@mylesbraithwaite.null'
+            },
             base: 'build',
             branch: 'master'
         },
