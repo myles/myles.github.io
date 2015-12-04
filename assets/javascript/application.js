942b4dbe8a30b5dd2fc57a656710e57744744e7f
Deploy <me+deploy@mylesbraithwaite.null>
2015-12-03 21:48:24 -0500

    Updates

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,16 +1,20 @@
 (function() {
   $(function() {
     var articleArchiveRepo, articleRepo, source, template;
-    source = '<ul class="bullets">' + '{{#each data}}' + '{{#unless fork}}' + '<li class="bullet">' + '<div class="bullet-content">' + '<a href="{{html_url}}">' + '<h2>' + '{{#if language}}{{language}}: {{/if}}' + '{{full_name}} ' + '</h2>' + '</a>' + '<ul class="bullet-links">' + '<li>' + '<a href="{{html_url}}">' + '<img src="/assets/images/octoface.svg" title="GitHub">' + '</a>' + '</li>' + '{{#if homepage}}' + '<li>' + '<a href="{{homepage}}">' + '<img src="/assets/images/link-external.svg" title="Homepage">' + '</a>' + '</li>' + '{{/if}}' + '{{#if has_wiki}}' + '<li>' + '<a href="{{html_url}}/wiki">' + '<img src="/assets/images/book.svg" title="Wiki">' + '</a>' + '</li>' + '{{/if}}' + '{{#if has_issues}}' + '<li>' + '<a href="{{html_url}}/issues">' + '<img src="/assets/images/issue-opened.svg" title="Wiki">' + '</a>' + '</li>' + '{{/if}}' + '</ul>' + '<p>{{description}}</p>' + '</div>' + '</li>' + '{{/unless}}' + '{{/each}}' + '</ul>';
+    source = '<ul class="bullets">' + '{{#each data}}' + '{{#unless fork}}' + '<li class="bullet">' + '<div class="bullet-content">' + '<a href="{{html_url}}">' + '<h2>' + '{{#if language}}{{language}}: {{/if}}' + '{{name}} ' + '</h2>' + '</a>' + '<ul class="bullet-links">' + '<li>' + '<a href="{{html_url}}">' + '<img src="/assets/images/octoface.svg" title="GitHub">' + '</a>' + '</li>' + '{{#if homepage}}' + '<li>' + '<a href="{{homepage}}">' + '<img src="/assets/images/link-external.svg" title="Homepage">' + '</a>' + '</li>' + '{{/if}}' + '{{#if has_wiki}}' + '<li>' + '<a href="{{html_url}}/wiki">' + '<img src="/assets/images/book.svg" title="Wiki">' + '</a>' + '</li>' + '{{/if}}' + '{{#if has_issues}}' + '<li>' + '<a href="{{html_url}}/issues">' + '<img src="/assets/images/issue-opened.svg" title="Wiki">' + '</a>' + '</li>' + '{{/if}}' + '</ul>' + '<p>{{description}}</p>' + '</div>' + '</li>' + '{{/unless}}' + '{{/each}}' + '</ul>';
     template = Handlebars.compile(source);
     articleRepo = $('#js-github-projects');
     articleArchiveRepo = $('#js-github-archive-projects');
-    jQuery.getJSON('https://api.github.com/users/myles/repos?sort=updated&type=all&callback=?', function(data) {
-      articleRepo.html(template(data));
-    });
-    jQuery.getJSON('https://api.github.com/orgs/myles-archive/repos?sort=updated&type=all&callback=?', function(data) {
-      articleArchiveRepo.html(template(data));
-    });
+    if (articleRepo) {
+      jQuery.getJSON('https://api.github.com/users/myles/repos?sort=updated&type=all&callback=?', function(data) {
+        articleRepo.html(template(data));
+      });
+    }
+    if (articleArchiveRepo) {
+      jQuery.getJSON('https://api.github.com/orgs/myles-archive/repos?sort=updated&type=all&callback=?', function(data) {
+        articleArchiveRepo.html(template(data));
+      });
+    }
   });
 
 }).call(this);
